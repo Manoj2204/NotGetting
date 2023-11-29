042688c5e18cea44d03777a414874e7052fcc4c0
Manoj2204 <mahtomanojkumar824@gmail.com>
2023-11-29 16:41:38 +0530

    first commit

---
+++ src/test/java/test/GetPageTitleTest.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class GetPageTitleTest 
+{
+	@Test
+	public void test()
+
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com");
+		String title = driver.getTitle();
+		System.out.println(title);
+		driver.quit();
+	}
+
+}
